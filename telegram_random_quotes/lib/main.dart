3acb6c8ac515f99a8bef2073290a086b392b22a0
thiepcong <thiepcong12@gmail.com>
2024-11-11 13:47:14 +0700

    update readme file

---
+++ telegram_random_quotes/lib/main.dart
@@ -0,0 +1,114 @@
+import 'dart:io';
+
+import 'package:http_client_hoc081098/http_client_hoc081098.dart';
+import 'package:http/http.dart' as http;
+import 'package:rxdart_ext/rxdart_ext.dart';
+
+void main() async {
+  final ci = Platform.environment['CI'] == 'true';
+  final chatIds = Platform.environment['TELEGRAM_CHAT_IDS']?.split(',');
+  final botToken = Platform.environment['TELEGRAM_BOT_TOKEN'];
+
+  if (chatIds == null ||
+      chatIds.isEmpty ||
+      botToken == null ||
+      botToken.isEmpty) {
+    print(
+        'TELEGRAM_CHAT_IDS or TELEGRAM_BOT_TOKEN environment variable is not set');
+    exit(1);
+  }
+
+  final loggingInterceptor = SimpleLoggingInterceptor(
+    SimpleLogger(
+      loggerFunction: print,
+      level: ci ? SimpleLogLevel.basic : SimpleLogLevel.body,
+    ),
+  );
+
+  final simpleHttpClient = SimpleHttpClient(
+    client: http.Client(),
+    timeout: const Duration(seconds: 20),
+    requestInterceptors: [
+      loggingInterceptor.requestInterceptor,
+    ],
+    responseInterceptors: [
+      loggingInterceptor.responseInterceptor,
+    ],
+  );
+
+  // await simpleHttpClient.getJson(
+  //   Uri.https(
+  //     'api.telegram.org',
+  //     '/bot$botToken/getUpdates',
+  //   ),
+  // );
+  //
+  // print('-' * 80);
+
+  await getQuote(simpleHttpClient)
+      .exhaustMap(
+        (quote) => Stream.fromIterable(chatIds).asyncExpand(
+          (chatId) => send(
+            simpleHttpClient: simpleHttpClient,
+            quote: quote,
+            chatId: chatId,
+            botToken: botToken,
+          )
+              .doOnError((e, s) => print('Error: $e, Stacktrace: $s'))
+              .onErrorResumeNext(Stream.empty()),
+        ),
+      )
+      .forEach((_) {});
+
+  simpleHttpClient.close();
+}
+
+Single<Quote> getQuote(SimpleHttpClient simpleHttpClient) =>
+    useCancellationToken(
+      (cancelToken) => simpleHttpClient.getJson(
+        Uri.parse('https://zenquotes.io/api/random'),
+        cancelToken: cancelToken,
+      ),
+    ).map((json) => Quote.fromJson(json[0]));
+
+Single<void> send({
+  required SimpleHttpClient simpleHttpClient,
+  required Quote quote,
+  required String chatId,
+  required String botToken,
+}) =>
+    useCancellationToken((cancelToken) {
+      final uri = Uri.https(
+        'api.telegram.org',
+        '/bot$botToken/sendMessage',
+        {
+          'chat_id': chatId,
+          'text': '''
+**${quote.quote}** - _${quote.author}_
+Have a nice day ❤️!
+-------------------
+- This message is sent by a bot
+- Source code: [telegram_random_quotes]
+      ''',
+          'parse_mode': 'Markdown',
+        },
+      );
+
+      return simpleHttpClient.getJson(
+        uri,
+        cancelToken: cancelToken,
+      );
+    });
+
+class Quote {
+  final String quote;
+  final String author;
+
+  Quote({
+    required this.quote,
+    required this.author,
+  });
+
+  factory Quote.fromJson(Map<String, dynamic> json) =>
+      Quote(quote: json['q'], author: json['a']);
+}
